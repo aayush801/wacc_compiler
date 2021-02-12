d7605e7b959e986d176da8107599c92ffaa24c61
Raul <rp519@ic.ac.uk>
2021-02-12 14:07:03 +0000

    merged ErrorCodes into the WaccCompiler file.

--- src/main/java/WaccCompiler.java
+++ src/main/java/WaccCompiler.java
@@ -104,3 +104,25 @@ public class WaccCompiler {
   }
 
 }
+
+enum ErrorCode {
+  SUCCESS,
+  SYNTAX_ERROR,
+  SEMANTIC_ERROR,
+  FAIL;
+
+  public int code() {
+    switch (this) {
+      case SUCCESS:
+        return 0;
+      case SYNTAX_ERROR:
+        return 100;
+      case SEMANTIC_ERROR:
+        return 200;
+      case FAIL:
+      default:
+        return -1;
+    }
+  }
+
+}
